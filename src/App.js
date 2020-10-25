2f5daf7df2a157852be67070521f4fc76dc1e838
samir0067 <arrahmani.samir@gmail.com>
2020-10-25 07:29:38 +0100

    init maps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow
+} from '@react-google-maps/api'
+import { formatRelative } from 'date-fns'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import mapStyles from "./mapStyles";
+
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption
+} from '@reach/combobox'
+import '@reach/combobox/styles.css'
+
+const libraries = ["places"]
+const mapContainerStyle = {
+  width: '100vw',
+  height: '90vh'
+}
+const center = {
+  lat: 48.573406,
+  lng: 7.752111,
+}
+const options = {
+  styles: mapStyles,
 }
 
-export default App;
+export default function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "REACT_APP_GOOGLE_API_KEY",
+    libraries,
+  })
+
+  if (loadError) return 'Error loading Maps'
+  if (!isLoaded) return 'Loading Maps'
+
+  return (
+    <div className="App">
+      <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={17}
+          center={center}
+          options={options}
+      >
+      </GoogleMap>
+    </div>
+  )
+}
